128262422fd6a55a042b71bb390e904c150a6be4
YukariYamaguchi4810 <tamazusa.mos@gmail.com>
2022-07-12 11:33:48 +0900

    プロジェクト ファイルを追加します。

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AppSettings.Pages;
+
+public class IndexModel : PageModel
+{
+    private readonly ILogger<IndexModel> _logger;
+
+    //↓↓↓追加
+    private readonly IConfiguration _configuration;
+
+    public String SampleValue { get; set; }
+    //↑↑↑追加
+
+    //↓↓↓引数追加
+    public IndexModel(ILogger<IndexModel> logger, IConfiguration configuration)
+    {
+    //↑↑上引数追加
+        _logger = logger;
+        //↓↓↓追加
+        _configuration = configuration;
+        //↑↑↑追加
+    }
+
+    public void OnGet()
+    {
+        //↓↓↓追加
+        SampleValue = _configuration["sampleKey"];
+        //↑↑↑追加
+    }
+}
+
